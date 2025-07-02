Tosainu/gnss-7-seg-clock

commit b24db0d2412d50fd361581d2e64102bfcc1be36a

Replace NMEA parser

crates/gnss-7-seg-clock/src/events.rs
@@ -5,6 +5,8 @@ use embassy_time::Timer;
 
 use chrono::NaiveDateTime;
 
+use crate::max_m10s::Event as MaxM10sEvent;
+
 pub enum Event {
     DateTimeUpdated(NaiveDateTime),
     Sw3Pressed,
@@ -14,7 +16,7 @@ pub enum Event {
 }
 
 pub struct EventSources<'d, M: RawMutex, const N: usize> {
-    receiver_nmea: Receiver<'d, M, nmea::ParseResult, N>,
+    receiver_nmea: Receiver<'d, M, MaxM10sEvent, N>,
     gpio_sw3: DebouncedInput<'d>,
     gpio_sw4: DebouncedInput<'d>,
     gpio_sw5: DebouncedInput<'d>,
@@ -24,7 +26,7 @@ pub struct EventSources<'d, M: RawMutex, const N: usize> {
 
 impl<'d, M: RawMutex, const N: usize> EventSources<'d, M, N> {
     pub fn new(
-        receiver_nmea: Receiver<'d, M, nmea::ParseResult, N>,
+        receiver_nmea: Receiver<'d, M, MaxM10sEvent, N>,
         gpio_sw3: gpio::Input<'d>,
         gpio_sw4: gpio::Input<'d>,
         gpio_sw5: gpio::Input<'d>,
@@ -51,16 +53,7 @@ impl<'d, M: RawMutex, const N: usize> EventSources<'d, M, N> {
             )
             .await
             {
-                Either5::First(msg) => {
-                    defmt::debug!("{}", msg);
-                    if let nmea::ParseResult::RMC(data) = msg {
-                        if let (Some(date), Some(time)) = (data.fix_date, data.fix_time) {
-                            let t = date.and_time(time);
-                            self.datetime = Some(t);
-                            return Event::DateTimeUpdated(t);
-                        }
-                    }
-                }
+                Either5::First(MaxM10sEvent::DateTime(t)) => return Event::DateTimeUpdated(t),
                 Either5::Second(..) => return Event::Sw3Pressed,
                 Either5::Third(..) => return Event::Sw4Pressed,
                 Either5::Fourth(..) => return Event::Sw5Pressed,

Cargo.toml
@@ -12,7 +12,7 @@ edition = "2024"
 [workspace.dependencies.misc]
 path = "crates/misc"
 
-[workspace.dependencies.nmea-wip]
+[workspace.dependencies.nmea]
 path = "crates/nmea"
 
 [workspace.dependencies.chrono]
@@ -75,22 +75,6 @@ features = ["defmt"]
 [workspace.dependencies.embedded-io-async]
 version = "0.6.1"
 features = ["defmt-03"]
-
-[workspace.dependencies.nmea]
-git = "https://github.com/AeroRust/nmea.git"
-rev = "17fb066fa2d4e5b92dcfd72627e83a07930327de"
-default-features = false
-features = [
-  "defmt-03",
-  "GGA",
-  "GLL",
-  "GSA",
-  "GST",
-  "GSV",
-  "RMC",
-  "TXT",
-  "VTG",
-]
 
 [workspace.dependencies.nom]
 version = "8.0.0"

crates/gnss-7-seg-clock/Cargo.toml
@@ -8,6 +8,10 @@ default-run = "gnss-7-seg-clock"
 [dependencies.misc]
 workspace = true
 
+[dependencies.nmea]
+features = ["defmt"]
+workspace = true
+
 [dependencies.chrono]
 workspace = true
 
@@ -42,9 +46,6 @@ workspace = true
 workspace = true
 
 [dependencies.embedded-io-async]
-workspace = true
-
-[dependencies.nmea]
 workspace = true
 
 [dependencies.panic-probe]

crates/gnss-7-seg-clock/src/main.rs
@@ -20,7 +20,7 @@ use gnss_7_seg_clock::{
     display::{self, Display},
     events::*,
     flash::NonVolatileConfig,
-    max_m10s::MaxM10s,
+    max_m10s::{Event as MaxM10sEvent, MaxM10s},
 };
 
 use {defmt_rtt as _, panic_probe as _};
@@ -30,7 +30,7 @@ embassy_rp::bind_interrupts!(struct Irqs {
     UART1_IRQ => uart::BufferedInterruptHandler<UART1>;
 });
 
-type NmeaChannel = Channel<ThreadModeRawMutex, nmea::ParseResult, 16>;
+type NmeaChannel = Channel<ThreadModeRawMutex, MaxM10sEvent, 8>;
 
 //
 //     +- A -+

crates/gnss-7-seg-clock/src/max_m10s.rs
@@ -4,6 +4,8 @@ use embassy_sync::{blocking_mutex::raw::RawMutex, channel::Sender};
 use embassy_time::{Duration, Instant, Timer};
 use embedded_io_async::Read;
 
+use chrono::NaiveDateTime;
+
 use misc::crlf_stream::CrlfStream;
 
 pub fn ubx_fill_ck(buf: &mut [u8]) {
@@ -41,6 +43,10 @@ enum State {
     Ready,
 }
 
+pub enum Event {
+    DateTime(NaiveDateTime),
+}
+
 impl<'d, Uart, I2c> MaxM10s<'d, Uart, I2c>
 where
     Uart: uart::Instance,
@@ -76,10 +82,7 @@ where
         }
     }
 
-    pub async fn run<M: RawMutex, const N: usize>(
-        &mut self,
-        sender: Sender<'_, M, nmea::ParseResult, N>,
-    ) {
+    pub async fn run<M: RawMutex, const N: usize>(&mut self, sender: Sender<'_, M, Event, N>) {
         let mut state = State::PowerCycle;
         loop {
             let next_state = match state {
@@ -205,7 +208,7 @@ where
 
     async fn do_reveive_nmea<M: RawMutex, const N: usize>(
         &mut self,
-        sender: &Sender<'_, M, nmea::ParseResult, N>,
+        sender: &Sender<'_, M, Event, N>,
     ) -> State {
         let mut buf = CrlfStream::<512>::new();
         let mut errors = 0_u32;
@@ -225,8 +228,16 @@ where
             }
 
             while let Some(line) = buf.pop() {
-                match nmea::parse_bytes(line) {
-                    Ok(msg) => sender.send(msg).await,
+                defmt::debug!("{:a}", line);
+                match nmea::parser::parse(line) {
+                    Ok(msg) => {
+                        if let nmea::parser::MessageType::Rmc(data) = msg.data {
+                            defmt::info!("{}", data);
+                            if let (Some(date), Some(time)) = (data.date, data.time) {
+                                sender.send(Event::DateTime(date.and_time(time))).await;
+                            }
+                        }
+                    }
                     Err(err) => defmt::warn!("{:a}: {}", line, err),
                 }
             }

commit 8dd8b079f6af171f4be0c3479ce5b8804a6d0d8c

Cache the latest time
This fixes regressin introduced in b24db0d2412d50fd361581d2e64102bfcc1be36a

crates/gnss-7-seg-clock/src/events.rs
@@ -54,7 +54,10 @@ impl<'d, M: RawMutex, const N: usize> EventSources<'d, M, N> {
             )
             .await
             {
-                Either5::First(MaxM10sEvent::DateTime(t)) => return Event::DateTimeUpdated(t),
+                Either5::First(MaxM10sEvent::DateTime(t)) => {
+                    self.datetime = Some(t);
+                    return Event::DateTimeUpdated(t);
+                }
                 Either5::Second(..) => return Event::Sw3Pressed,
                 Either5::Third(..) => return Event::Sw4Pressed,
                 Either5::Fourth(..) => return Event::Sw5Pressed,